67c258d96381a41ba8a74d8391000eeb20fd262f
yong <tkddbs1644@gmail.com>
2023-12-11 02:28:33 +0900

    bug: module import fail

--- apps/webapp/src/App.tsx
+++ apps/webapp/src/App.tsx
@@ -1,5 +1,26 @@
+import { Button } from '@core/ui';
+import { Say } from '@core/lib';
+import { useState } from 'react';
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const [state, setState] = useState(0);
+
+  const increase = () => {
+    setState(prev => prev + 1);
+  };
+
+  const handleClick = () => {
+    window.alert(Say.say('Hello!'));
+  };
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Button onClick={handleClick}>hi</Button>
+      <h2>{state}</h2>
+      <Button onClick={increase}></Button>
+    </>
+  );
 }
 
 export default App;
